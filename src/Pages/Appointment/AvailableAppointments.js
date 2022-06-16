38785189981bc1d9313faa69c22047097b7beda2
MD Limon Farazi <abidislamabir@gmail.com>
2022-06-16 23:01:26 +0600

    modal added

--- src/Pages/Appointment/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments.js
@@ -1,10 +1,15 @@
 import React,{useState,useEffect} from 'react';
 import { format } from 'date-fns';
 import Booking from './Booking';
+import BookingModal from './BookingModal';
 
 const AvailableAppointments = ({date}) => {
 
   const [services,setServices] = useState([]);
+  const [treatment,setTreatment] = useState([]);
+  // console.log(treatment);
+
+
   useEffect( ()=>{
     fetch('services.json')
     .then(res => res.json())
@@ -16,8 +21,9 @@ const AvailableAppointments = ({date}) => {
       <h1 className="text-center font-bold capitalize text-secondary text-xl">Available Appointment on {format(date, 'PP')}</h1>
       <h3 className="text-center capitalize mt-3 text-xl">Please select a service</h3>
     <div className='px-6 mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10'>
-      {services.map(service => <Booking key={service._id} service={service}/>)}
+      {services.map(service => <Booking key={service._id} setTreatment={setTreatment} service={service}/>)}
     </div>
+    {treatment && <BookingModal treatment={treatment}/>}
     </div>
   );
 };
--- src/Pages/Appointment/Booking.js
+++ src/Pages/Appointment/Booking.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PrimaryButton from "../Shared/PrimaryButton";
 
-const Booking = ({service}) => {
+const Booking = ({setTreatment,service}) => {
   const {name,slots} = service;
   return (
     <div className='text-center'>
@@ -12,7 +12,8 @@ const Booking = ({service}) => {
     <p className="mb-1">{slots.length > 0 ? <p>{slots[0]}</p> : <p className="text-red-500 font-bold">try another day</p>}</p>
     <p className="mt-2">{slots.length} {slots.length > 1 ? 'Spaces' : 'Space'} Available</p></div>
     <div class="card-actions justify-center">
-      <button disabled={slots.length === 0} className="text-white font-bold btn btn-primary">Book Now</button>
+      {/* <button disabled={slots.length === 0} className="text-white font-bold btn btn-primary">Book Now</button> */}
+      <label onClick={()=>setTreatment(service)} disabled={slots.length === 0} for="booking-modal" class="text-white font-bold btn btn-primary">Book Appointment</label>
     </div>
   </div>
 </div>
