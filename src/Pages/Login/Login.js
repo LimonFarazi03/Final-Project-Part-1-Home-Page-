312ae31dc24729d442932785de0f45f35e0db8c0
MD Limon Farazi <abidislamabir@gmail.com>
2022-06-18 22:36:55 +0600

    loading error and spinner added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,100 +1,110 @@
 import React from "react";
-import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { useSignInWithGoogle,useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
 import auth from "../../firebase.init";
 import { useForm } from "react-hook-form";
 
-{
-  /* <input {...register("lastName", { required: true })} /> */
-}
-
 const Login = () => {
-  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
+  const [signInWithEmailAndPassword,user,loading,error,] = useSignInWithEmailAndPassword(auth);
   const {
     register,
     formState: { errors },
     handleSubmit,
   } = useForm();
+  // Loading
+  if(gLoading || loading){
+    return <div className="flex h-screen justify-center items-center"> <progress class="progress w-56"></progress> </div>
+  };
+  // Error message
+  let signInError;
+  if(error || gError){
+    signInError = <p className='text-red-500 text-center mt-2 text-sm'>⚠️ <small>{error?.message || gError?.message}</small></p>
+  }
+
   const onSubmit = (data) => {
     console.log(data);
+    signInWithEmailAndPassword(data.email, data.password);
   };
+
+
   return (
     <div className="flex h-screen justify-center items-center">
-      <div class="card w-96 bg-base-100 shadow-xl">
-        <div class="card-body">
-          <h2 class="text-center text-secondary uppercase font-bold text-xl">
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <div className="card-body">
+          <h2 className="text-center text-secondary uppercase font-bold text-xl">
             Login
           </h2>
           <form className="grid grid-cols-1" onSubmit={handleSubmit(onSubmit)}>
             {/* email input */}
-            <div class="form-control w-full max-w-xs">
-              <label class="label">
-                <span class="label-text">Email</span>
+            <div className="form-control w-full max-w-xs">
+              <label className="label">
+                <span className="label-text">Email</span>
               </label>
               <input
                 type="email"
                 placeholder="your email here"
-                class="input input-bordered w-full max-w-xs"
+                className="input input-bordered w-full max-w-xs"
                 {...register("email", {
                   required: {
                     value: true,
-                    message: "email is required",
+                    message: "⚠️ email is required",
                   },
                   pattern: {
                     value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
-                    message: "provide a valid email",
+                    message: "⚠️ provide a valid email",
                   },
                 })}
               />
-              <label class="label">
+              <label className="label">
                 {errors.email?.type === "required" && (
-                  <span class="label-text-alt text-red-500">
+                  <span className="label-text-alt text-red-500">
                     {errors.email.message}
                   </span>
                 )}
                 {errors.email?.type === "pattern" && (
-                  <span class="label-text-alt text-red-500">
+                  <span className="label-text-alt text-red-500">
                     {errors.email.message}
                   </span>
                 )}
               </label>
             </div>
             {/* Password input */}
-            <div class="form-control w-full max-w-xs">
-              <label class="label">
-                <span class="label-text">Password</span>
+            <div className="form-control w-full max-w-xs">
+              <label className="label">
+                <span className="label-text">Password</span>
               </label>
               <input
                 type="password"
                 placeholder="password here"
-                class="input input-bordered w-full max-w-xs"
+                className="input input-bordered w-full max-w-xs"
                 {...register("password", {
                   required: {
                     value: true,
-                    message: "password is required",
+                    message: "⚠️ password is required",
                   },
                   minLength: {
                     value: 7,
-                    message: "password will be 7 character or long",
+                    message: "⚠️ password will be 7 character or long",
                   },
                   pattern: {
                     value: /(?=.*[!#$%&?^*@~() "])(?=.{8,})/,
-                    message: "your password must be strong improve now",
+                    message: "⚠️ your password must be strong improve now",
                   },
                 })}
               />
-              <label class="label">
+              <label className="label">
                 {errors.password?.type === "required" && (
-                  <span class="label-text-alt text-red-500">
+                  <span className="label-text-alt text-red-500">
                     {errors.password.message}
                   </span>
                 )}
                 {errors.password?.type === "minLength" && (
-                  <span class="label-text-alt text-red-500">
+                  <span className="label-text-alt text-red-500">
                     {errors.password.message}
                   </span>
                 )}
                 {errors.password?.type === "pattern" && (
-                  <span class="label-text-alt text-red-500">
+                  <span className="label-text-alt text-red-500">
                     {errors.password.message}
                   </span>
                 )}
@@ -106,9 +116,10 @@ const Login = () => {
               value="login"
               className="mt-4 bg-gradient-to-r from-secondary to-primary font-bold text-white	btn btn-primary"
             />
+            {signInError}
           </form>
           <div className="divider">OR</div>
-          <button onClick={() => signInWithGoogle()} class="btn btn-outline">
+          <button onClick={() => signInWithGoogle()} className="btn btn-outline">
             Continue With Google
           </button>
         </div>
